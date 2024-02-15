e241ad1cb9e418504608eaa079664f0da3eb0c76
Georg <gwbootcamp24.google.com>
2024-02-15 22:13:10 +0100

    save und remove geht endlich

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ notiz
             this.setDeadline(deadline);
             return this
         }
+        setText(text){
+            this.text = text
+            return this
+        }
         setDeadline(date){
             this.readDate = date
             return this
@@ -43,16 +47,17 @@ notiz
         notes;
 
         constructor(){
+            this.title = 'Meine Todo-Liste'        
             this.notes = []         
-            this.htmlFormTemplate = `<form action="">
-            <input type="text" name="noteText" id="noteText" value="{--TEXT--}" >
-            <input type="datetime-local" id="deadline" name="deadline" value="{--DEADLINE--}" >
-            <div class="buttons">Save todo</div>
-            </form>
+            this.htmlFormTemplate = `<div class="item" data-item_id="{--DEADLINE--}">
+            <input type="text" name="noteText" class="noteText" value="{--TEXT--}" >
+            <input type="date" class="deadline" name="deadline" value="{--DEADLINEINPUT--}" >
+            <button class="save">Save todo</button>
+            </div>
             `;
             this.htmlTemplate = `
-            <div class="item" data-itemId="{--DEADLINE--}">
-                        <div class="content">{--TEXT--}</div><div class="deadline">{--DEADLINE--}</div><button class="edit">Edit</button><button class="delete">delete</button><button class="done">done</button><button class="done">Save Note</button>
+            <div class="item" data-item_id="{--DEADLINE--}">
+                        <div class="content">{--TEXT--}</div><div class="deadline">{--DEADLINE--}</div><button class="edit">Edit</button><button class="delete">delete</button><button class="done">done</button>
                     </div>
             `
         }   
@@ -65,8 +70,10 @@ notiz
         updateNodes() {
             this.sortNotes();
         }
-        remove(deadline) {
-            this.notes = this.notes.find((e) => e.deadline !== deadline);
+        remove(domNote) {
+            const itemId = domNote.dataset.item_id;
+            this.notes = this.notes.find((e) => e.deadline !== itemId);
+            domNote.remove();
 
         }
         update(text, done, deadline) {
@@ -76,52 +83,49 @@ notiz
         }
         printList() {
 
-            let output = `<div><div class="listTitle">${this.title}></div>`;
+            let output = `<div><div class="listTitle">${this.title}</div>`;
             output = output + this.notes.reduce((acc, key) => {
-                // console.log(acc);
                 acc = acc + this.htmlTemplate.replace('{--TEXT--}', key.text).replaceAll('{--DEADLINE--}', key.deadline);
                 return acc;
             })
             output * output + '</div>';
-            const positionSelect = document.querySelector(".listeWrapper");
+            const positionSelect = document.querySelector(".noteList");
             positionSelect.insertAdjacentHTML(
-              'afterend',
+              'beforeend',
               output,
             );
 
         }
         printNote() {
-            
         }
         sortNotes(){
             this.notes.sort((a, b) => a.deadline - b.deadline);
         }
 
         newEditNote(deadline) {
-
-            const noteEdited = this.notes.find((e) => e.deadline === deadline);
-
-            let output = this.htmlFormTemplate.replace('{--TEXT--}', this.text).replace('{--DEADLINE--}', this.deadline.toDateString());
-            
-            const positionSelect = document.querySelector('[data-itemId="' + deadline + '"]');
-            positionSelect.insertAdjacentHTML(
-              'afterend',
-              output,
-            );
-                
-
+        }
+        insertNote(DOMPosition) {
         }
         editNote(deadline) {
 
+
             const noteEdited = this.notes.find((e) => e.deadline === deadline);
-            console.log('########' + deadline);
-            let output = this.htmlFormTemplate.replace('{--TEXT--}', this.text).replace('{--DEADLINE--}', this.deadline);
+
+            // // const input = document.querySelector('input')
+            // const dt = new Date(2021, 1, 1);
+            // const day = ("0" + dt.getDate()).slice(-2);
+            // const month = ("0" + (dt.getMonth() + 1)).slice(-2);
+            // const date = dt.getFullYear() + "-" + month + "-" + day;
+            // // input.value = date
+
+            let output = this.htmlFormTemplate.replace('{--TEXT--}', noteEdited.text).replaceAll('{--DEADLINE--}', noteEdited.deadline).replaceAll('{--DEADLINEINPUT--}', noteEdited.deadline.substring(0, 10));
             
-            const positionSelect = document.querySelector('[data-itemId="' + deadline + '"]');
+            const positionSelect = document.querySelector('[data-item_id="' + deadline + '"]');
             positionSelect.insertAdjacentHTML(
               'afterend',
               output,
             );
+
                 
 
         }
@@ -130,43 +134,113 @@ notiz
             noteToCheck.done = true;
             return this
         }
-        saveNote() {
+        
+        saveNote(domNote) {
+            const itemId = domNote.dataset.item_id;
+            const noteToBeSaved = this.notes.find((e) => e.deadline === itemId);
+            const date = domNote.querySelector('input[name="deadline"]');
+            const text = domNote.querySelector('.noteText');
+            const addedTime = new Date().toISOString().substring(10);
+            const newDeadline = date.value + addedTime
+            noteToBeSaved.setDeadline(newDeadline)
+            noteToBeSaved.setText(text);
+
+            let output = this.htmlTemplate.replace('{--TEXT--}', text.value).replaceAll('{--DEADLINE--}', newDeadline);
+            console.log(output);
+            
+            domNote.insertAdjacentHTML(
+                "beforebegin",
+                output,
+            );
+            domNote.remove();
+
+
+
             
         }
         connectEventListeners(){
 
-            function callFn (fnToBeCalled, args = []) {
-                fnToBeCalled(...args);
-            }
-        
-               
-            const editBtns = document.querySelectorAll('button.edit');
 
+            // function callFn (fnToBeCalled, args = []) {
+            //     fnToBeCalled(...args);
+            // }
 
-            editBtns.forEach((btn) => {
-                console.log(btn);
-                const noteId = btn.parentElement.dataset.itemid;
-                console.log(btn.parentElement.dataset);
-                console.log(noteId);
-
-                const buttonFnArr = {
-                    'delete': [this.remove, [noteId]
-                    ],
-                    'edit': [this.editNote,['Dirk Documentorerer']],
-                    'done': [this.checkDone,[noteId]]
+            const that = this;
+            function handleKeyboardKlick(e){
+                // if (exitCond) return;
+                const btn = e.target.closest("button");
+                const domNote = e.target.closest('.item')
+                if (!btn) return;
+                const buttonClass = btn.classList[0];
+                const itemId = domNote.dataset.item_id;
+                switch (buttonClass) {
+                    case 'edit': that.editNote(itemId); break;
+                    case 'delete': 
+                    if (confirm("Wirklich löschen?") === true) {
+                        that.remove(domNote); 
+                      } else {
+                      }
+                    break;
+                    case 'save': that.saveNote(domNote); break;
                 }
-                btn.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    this.editNote('Dirk Documentorerer')
-                });
-            })
+            }
+
+
+            // const editBtns = document.querySelectorAll('button.edit');
+            // const deleteBtns = document.querySelectorAll('button.delete');
+
+            document.querySelector('.noteList').addEventListener('click', handleKeyboardKlick);
+
+
+
+            // editBtns.forEach((btn) => {
+
+            //     const noteId = btn.parentElement.dataset.itemid;
+
+                
+            //     btn.addEventListener('click', (event) => {
+            //         event.preventDefault();
+            //         this.editNote(noteId)
+            //     });
+            // })
+            
+            // deleteBtns.forEach((btn) => {
+
+            //     const noteId = btn.parentElement.dataset.itemid;
+
+
+
+                
+            //     btn.addEventListener('click', (event) => {
+            //         event.preventDefault();
+            //         this.remove(noteId)
+            //     });
+            // })
+            
+
+
+
+                // const buttonFnArr = {
+                //     'delete': [this.remove, [noteId]
+                //     ],
+                //     'edit': [this.editNote,[noteId]],
+                //     'done': [this.checkDone,[noteId]]
+                // }
+
+
+                // const buttonFnArr = {
+                //     'delete': [this.remove, [noteId]
+                //     ],
+                //     'edit': [this.editNote,[noteId]],
+                //     'done': [this.checkDone,[noteId]]
+                // }
+
+
             
             // const allBtns = document.querySelectorAll('button:not(.addBtn)');
-            // console.log(allBtns);
 
             // allBtns.forEach((btn) => {
-            //     console.log(btn);
-            //     const noteId = btn.parentElement.dataset.itemId;
+            //     const noteId = btn.parentElement.dataset.item_id;
             //     const buttonFnArr = {
             //         'delete': [this.remove, [noteId]
             //         ],
@@ -178,7 +252,6 @@ notiz
             //   btn.addEventListener('click', (event) => {
             //     event.preventDefault();
             //     const fnToCall = buttonFnArr[btn.classList.item(0)][0];
-            //     console.log(fnToCall);
             //     callFn(fnToCall, buttonFnArr[btn.classList.item(0)][1]);
             //   });
             // })
@@ -195,15 +268,15 @@ notiz
 
     
  
-    const allBtns = document.querySelectorAll('button');
+    // const allBtns = document.querySelectorAll('button');
 
-    allBtns.forEach((btn) => {
-      btn.addEventListener('click', (event) => {
-        event.preventDefault();
-        const fnToCall = buttonFnArr[btn.classList.item(0)][0];
-        callFn(fnToCall, buttonFnArr[btn.classList.item(0)][1]);
-      });
-    })
+    // allBtns.forEach((btn) => {
+    //   btn.addEventListener('click', (event) => {
+    //     event.preventDefault();
+    //     const fnToCall = buttonFnArr[btn.classList.item(0)][0];
+    //     callFn(fnToCall, buttonFnArr[btn.classList.item(0)][1]);
+    //   });
+    // })
 
 
     // const saveNode = (noteId) => {
@@ -220,41 +293,43 @@ notiz
     <div class="buttons">Buttons</div>
     </div> */
 
+    // 2011-10-05T14:48:00.000Z
+
     const myNotesToBeStores =   [
     {
         text: "vulture of the stars",
         done: false, 
-        deadline: 'Nick Names'
+        deadline: '2024-02-17T12:12:12.123Z'
     },
     {
         text: "builders of earth",
         done: false, 
-        deadline: 'Dirk Documentorerer'
+        deadline: '2024-02-22T12:12:12.523Z'
     },
     {
         text: "bakers without duty",
         done: false, 
-        deadline: 'Cern Regulator'
+        deadline: '2024-02-17T12:12:12.143Z'
     },
     {
         text: "couriers and foreigners",
         done: false, 
-        deadline: 'Nick Names'
+        deadline: '2024-03-17T12:12:12.123Z'
     },
     {
         text: "medics and strangers",
         done: false, 
-        deadline: 'Nick Names'
+        deadline: '2024-02-17T12:12:12.124Z'
     },
     {
         text: "accidents without desire",
         done: false, 
-        deadline: 'Nick Names'
+        deadline: '2024-02-17T12:02:12.1234Z'
     },
     {
         text: "lord of the sea",
         done: false, 
-        deadline: 'Nick Names'
+        deadline: '2024-02-27T12:12:12.144Z'
     } 
     ]
 
@@ -265,7 +340,6 @@ notiz
 
     })      
 
-// console.log(myNoteList)
 myNoteList.printList();
 
 myNoteList.connectEventListeners();
